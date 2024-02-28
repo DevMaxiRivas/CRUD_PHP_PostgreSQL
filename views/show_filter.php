e2ae6d20937554e9fed69218bcdb03503ddfa63e
Laptop MR <maximilianorivas01@gmail.com>
2024-02-27 21:52:42 -0300

    2701 Front Filter

--- views/show_filter.php
+++ views/show_filter.php
@@ -1,11 +1,11 @@
 <?php
                     require __DIR__.'/../template/header.php';
                     if(isset($_GET['db']) && isset($_GET['name'])){
-                        echo "<h5 class='card-title'>Primeras 100 filas de ".$_GET['name']."</h5>";
+                        echo "<h5 class='card-title'>Filtrar la tabla ".$_GET['name']."</h5>";
                     }
                     else{
-                        echo "<h5 class='card-title'>No se pueden mostrar las filas</h5>";
-                    }
+                        echo "<h5 class='card-title'>Ocurrio un error al intentar filtrar</h5>";
+                    }                    
 
                     // Importamos los archivos necesarios para realizar una conexíon
                     require_once __DIR__."/../routes/bootstrap.php";
@@ -21,7 +21,7 @@
 
                         $columns = $conn->exec_query_db(
                             $db,
-                            "SELECT column_name 
+                            "SELECT column_name, data_type
                             FROM information_schema.columns 
                             WHERE 
                                 table_name = :table
@@ -29,53 +29,28 @@
                             ['table' => $table]                     
                         );
                         
-                        // Otenemos 100 filas
-                        $query = "SELECT " . $columns[0]['column_name'];
-                        foreach ($columns as $col){
-                            $query = $query.", {$col['column_name']}";
-                        }
-                        $query = $query." FROM {$table} LIMIT {$limit};";
-                        $rows = $conn->exec_query_db($db,$query);
-
-                        if ($columns && $rows) {
-                            // Creamos la tabla una vez consultemos a la BD obteniendo las columnas y las filas de la tabla
-                            echo "
-                            <div class='row' style=' height: 30em;width: 40em;overflow: auto;'>
-                                <table class='table table-sm table-bordered table-striped table-hover'>
-                                    <thead style='position: sticky; top: 0;'>";
-                                    
-                            // Creamos cabezal de la tabla, colocando como columnas todas las columnas de la tabla elegida
-                            echo "<tr>";
-                            foreach ($columns as $column) {
-                                echo "<th>{$column['column_name']}</th>";
-                            }
-                            echo "</tr>";
+                    // Creamos un formulario con la columnas de las tablas
+                        if ($columns) {
+                            echo '<form>';
                             
-                            // Cerramos la etiqueta thead
-                            echo '</thead>';
-
-                            // Cargamos el body de la tabla con las 100 primeras filas de la tabla seleccionada
-                            echo "<tbody id='tbody-rows'>";
-
-                            foreach ($rows as $row) {
-                                echo "<tr>";
-                                // Recorremos las columnas obteniendo asi el valor correspondiente para cada fila
-                                // Cada vez que vamos a recorrer las columnas debemos reiniciar el puntero mediante pg_result_seek
-
-                                foreach ($columns as $column) {
-                                    echo "<td>{$row[$column['column_name']]}</td>";
-                                }
-                                echo "</tr>";
+                            foreach ($columns as $col){
+                                echo 
+                                    '<div class="form-group">
+                                        <label for="'.$col['column_name'].'">'.$col['column_name'].'</label>
+                                        <input type="text" class="form-control" id="'.$col['column_name'].'" placeholder="Enter '.$col['column_name'].'">
+                                    </div>';
                             }
-                            // Una vez lleno el body, procedemos a cerrar las etiquetas
-                            echo '</tbody></table></div>';
-                        }
-                        else{
-                            // Si algunas de las consultas fallaron, se le notificara al usuario
+
+
+                        echo 
+                            '   <button type="submit" class="btn btn-primary">Submit</button>
+                            </form>';
+                        }else{
+                            // Si la consulta fallo, se le notificara al usuario
                             echo "
                                 <div class='alert alert-danger' style='heigth:5em;'>
                                     <p>
-                                        No se pudo obtener las columnas y/o las filas de la Tabla
+                                        No se pudo obtener las columnas de la Tabla
                                     </p>
                                 </div>";
                         }
